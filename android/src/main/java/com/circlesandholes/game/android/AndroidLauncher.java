5602925949e6014f41b7872a8b82fb1d754eb6c7
Artem Bolgov <abolgov@vmb-team.com>
2024-08-01 16:07:22 +0200

    update project

---
+++ android/src/main/java/com/circlesandholes/game/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.circlesandholes.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.circlesandholes.game.Intro;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Intro(), configuration);
+    }
+}
